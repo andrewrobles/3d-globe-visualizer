4ccd8b683e6e38f8d6c3c884cf92da2cc5eae324
Andrew Robles <andrewrobles@verizon.net>
2022-01-29 09:10:19 -0800

    Move fetch function call inside of useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,13 @@ function App() {
   })
 
   useEffect(() => {
-    getMessage()
-  }, [])
-
-  const getMessage = () => {
     fetch('http://localhost:8000/helloworld/')
     .then(response => response.json())
     .then(data => saveMessage(data.message))
     .catch( err => {
         saveMessage('Client could not fetch from server')
     })
-  }
+  }, [])
 
   const saveMessage = (message) => {
       setState({
