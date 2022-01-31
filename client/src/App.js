d02d3b79483038594ad37b4eb07b6fa622c8fd53
Andrew Robles <andrewrobles@verizon.net>
2022-01-31 05:46:45 -0800

    Render Globe component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,10 @@
-import {useState, useEffect} from 'react'
+import Globe from 'worldwind-react-globe'
 
 function App() {
-  const [state, setState] = useState({
-    message: "",
-  })
-
-  useEffect(() => {
-    fetch('http://localhost:8000/helloworld/')
-    .then(response => response.json())
-    .then(data => saveMessage(data.message))
-    .catch( err => {
-        saveMessage('Client could not fetch from server')
-    })
-  }, [])
-
-  const saveMessage = (message) => {
-      setState({
-        message: message,
-      })
-  }
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      {state.message}
-    </h1>
+    <div>
+      <Globe />
+    </div>
   );
 }
 
