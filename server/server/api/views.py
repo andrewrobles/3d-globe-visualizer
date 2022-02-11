cd872566339fa9a94033911b199be8b9ed066149
Andrew Robles <andrewrobles@verizon.net>
2022-02-11 07:32:22 -0800

    Remove DRF browsable API

--- server/server/api/views.py
+++ server/server/api/views.py
@@ -45,24 +45,3 @@ def get_markers():
             'altitude': m.altitude
         } for m in Marker.objects.all()
     ])
-
-class MarkerViewSet(viewsets.ModelViewSet):
-    queryset = Marker.objects.all()
-    serializer_class = MarkerSerializer
-    permission_classes = [permissions.AllowAny]
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,19 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
-
-from rest_framework import routers
 from server.api import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-# Note the marker/ viewset pattern here vs. markers/ in urlpatterns
-# This was done to prevent overwriting the CRUD functionality in views.py
-router.register(r'marker', views.MarkerViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('markers/', views.handle_markers_request, name='handle-markers-request'),
     path('markers/<int:pk>', views.handle_markers_request, name='handle-markers-request'),
