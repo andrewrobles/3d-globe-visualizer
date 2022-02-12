f54ecdffd47bb8fd76f6aa27a521e3e80237dc23
Andrew Robles <andrewrobles@verizon.net>
2022-02-12 07:26:38 -0800

    Add marker admin list display attributes

--- server/server/api/admin.py
+++ server/server/api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Marker
 
-admin.site.register(Marker)
+@admin.register(Marker)
+class MarkerAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'latitude', 'longitude', 'altitude')
--- server/server/api/models.py
+++ server/server/api/models.py
@@ -4,3 +4,6 @@ class Marker(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=5)
     longitude = models.DecimalField(max_digits=9, decimal_places=5)
     altitude = models.DecimalField(max_digits=9, decimal_places=5)
+
+    def __str__(self):
+        return 'Marker object ({})'.format(self.id)
